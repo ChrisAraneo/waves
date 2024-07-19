0b62f7f23226760a058f5a48c3228d104f9d1502
Krzysztof Pająk <chris.araneo@gmail.com>
2024-07-19 16:54:24 +0200

    Update param name

--- src/functions/write.ts
+++ src/functions/write.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
-export async function write(input: { css: string; path: string }): Promise<void> {
+export async function write(input: { content: string; path: string }): Promise<void> {
   return new Promise((resolve, reject) => {
-    fs.writeFile(input.path, input.css, (error) => {
+    fs.writeFile(input.path, input.content, (error) => {
       if (error) {
         reject(error);
       }
